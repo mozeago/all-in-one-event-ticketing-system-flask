3faf289dd4ccd48630013a3788b601110ba601d9
Moses Asiago <mozzeago@gmail.com>
2022-09-25 03:13:20 +0300

    added project config classes

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from sqlalchemy import true
 app = Flask(__name__)
 CORS(app)
+incomes = [
+    {'description': 'salary', 'amount': 5000}
+]
 
 
-@app.route("/greeting")
+@app.route("/")
 def greeting():
-    return {"greeting": "Hello from Flask"}
+    return jsonify(incomes)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=true)
